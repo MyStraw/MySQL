b9f68cf45b44966510a43ec904287a01976e173a
MyStraw <kikongdosa@gmail.com>
2023-04-28 12:58:37 +0900

    4월 28일 구구단까지 끝

--- 냐하하/4월28일 수업3,4.sql
+++ 냐하하/4월28일 수업3,4.sql
@@ -1,6 +1,13 @@
 -- cross join
 
-select concat( b.v, " x " , a.v  ," = " , b.v*a.v) as result
+select concat( b.v, " x " , a.v  ," = " , b.v*a.v) as 2단, 
+concat(c.v, " x " , a.v  ," = " , b.v*a.v) as 3단,
+concat(d.v, " x " , a.v  ," = " , b.v*a.v) as 4단,
+concat(e.v, " x " , a.v  ," = " , b.v*a.v) as 5단,
+concat(f.v, " x " , a.v  ," = " , b.v*a.v) as 6단,
+concat(g.v, " x " , a.v  ," = " , b.v*a.v) as 7단,
+concat(h.v, " x " , a.v  ," = " , b.v*a.v) as 8단,
+concat(i.v, " x " , a.v  ," = " , b.v*a.v) as 9단
 from (
 	select 1 v union
 	select 2 v union
@@ -12,4 +19,18 @@ from (
 	select 8 v union
 	select 9 v)a
 join
-	(select 2 v)b;
+	(select 2 v ) b
+join
+	(select 3 v) c
+join
+	(select 4 v) d
+join
+	(select 5 v) e
+join
+	(select 6 v) f
+join
+	(select 7 v) g
+join
+	(select 8 v) h
+join
+	(select 9 v) i;
